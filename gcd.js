82685fec90f32dd5ef83d9da4a04f5480598b68d
natlou <natloudev@gmail.com>
2022-08-20 22:05:17 -0700

    gcd

---
+++ gcd.js
@@ -0,0 +1,15 @@
+function gcd(testVariable1, testVariable2) {
+
+    if (testVariable1 == testVariable2) {
+      return testVariable1;
+    }
+  
+    if (testVariable1 > testVariable2) {
+      return gcd(testVariable1 - testVariable2, testVariable2);
+    }
+    
+    else {
+      return gcd(testVariable1, testVariable2 - testVariable1);
+    }
+    
+}
