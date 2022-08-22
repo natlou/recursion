31478a619d03b108b2af8b334b3653ae9dcf86db
natlou <natloudev@gmail.com>
2022-08-21 21:59:27 -0700

    pascal

---
+++ pascal.js
@@ -0,0 +1,17 @@
+function printPascal(testVariable) {
+    if (testVariable == 0) {
+        return [1];
+    }
+  
+    else {
+      var line = [1];
+      previousLine = printPascal(testVariable - 1);
+  
+      for (let i = 0; i < previousLine.length - 1; i++) {
+        line.push(previousLine[i] + previousLine[i + 1]);
+      }
+  
+      line.push(1);
+    } 
+    return line;
+}
